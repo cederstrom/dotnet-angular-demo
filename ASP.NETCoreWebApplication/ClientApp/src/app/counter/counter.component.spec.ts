514c3927248968e9d0325afa62727bfd600907b7
Andreas Cederström <andreas.cederstrom@factor10.com>
2019-11-14 11:20:21 +0100

    Added call to server when counter is updated

--- ASP.NETCoreWebApplication/ClientApp/src/app/counter/counter.component.spec.ts
+++ ASP.NETCoreWebApplication/ClientApp/src/app/counter/counter.component.spec.ts
@@ -1,19 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CounterComponent } from './counter.component';
+import { InjectionToken } from '@angular/core';
 
 describe('CounterComponent', () => {
+  const baseUrl = "http://mocked/";
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CounterComponent
+      ],
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: "BASE_URL", useValue: baseUrl },
+      ],
+    });
+
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,14 +31,42 @@ describe('CounterComponent', () => {
     expect(titleText).toEqual('Counter');
   });
 
-  test('should start with count 0, then increments by 1 when clicked', async () => {
-    const countElement = fixture.nativeElement.querySelector('strong');
-    expect(countElement.textContent).toEqual('0');
+  describe("when clicking increment button", () => {
+    let countElement;
 
-    const incrementButton = fixture.nativeElement.querySelector('button');
-    incrementButton.click();
-    fixture.detectChanges();
-    expect(countElement.textContent).toEqual('1');
+    beforeEach(() => {
+      countElement = fixture.nativeElement.querySelector('strong');
+      expect(countElement.textContent).toEqual('0');
+
+      const incrementButton = fixture.nativeElement.querySelector('button');
+      incrementButton.click();
+      fixture.detectChanges();
+    });
+
+    describe("UI", () => {
+      test("shall update counter", () => {
+        expect(countElement.textContent).toBe('1');
+      });
+    });
+
+    describe("server", () => {
+      let httpMock: HttpTestingController;
+
+      beforeEach(() => {
+        httpMock = TestBed.get(HttpTestingController);
+      });
+
+      afterEach(() => {
+        httpMock.verify();
+      });
+
+      test("shall have been called with new counter value", () => {
+        const mock = httpMock.expectOne(`${baseUrl}api/counter`);
+        expect(mock.request.method).toEqual("PUT");
+        expect(mock.request.body).toEqual({ count: 1 });
+      });
+    });
+
   });
 
 });
--- ASP.NETCoreWebApplication/ClientApp/src/app/counter/counter.component.ts
+++ ASP.NETCoreWebApplication/ClientApp/src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-counter-component',
@@ -8,7 +9,24 @@ import { Component } from '@angular/core';
 export class CounterComponent {
   public currentCount = 0;
 
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+  private updateCounter() {
+    const counterData = new CounterData(this.currentCount);
+    const url = this.baseUrl + 'api/counter';
+    this.http.put<CounterData>(url, counterData).subscribe();
+  }
+
   public incrementCounter() {
     this.currentCount++;
+    this.updateCounter();
+  }
+}
+
+class CounterData {
+  count: number;
+
+  constructor(count: number) {
+    this.count = count;
   }
 }
